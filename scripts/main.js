45b8dd0d33b03fd3025ebf920cc896fb4a227ef6
Bodrul Alam <bodrul2112@gmail.com>
2013-02-16 01:06:21 +0000

    Added Optimizer
    and some decorum... : D

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,10 @@
 
 
-require( ["plugins/domReady"], function(domReady){
-    
-	
+require( ["plugins/domReady","thirdparty/jquery", "timeline/TimeLineController"], function(domReady, jQuery, TimeLineController){
 	
 	domReady(function(){
 		
 		require(["thirdparty/jquery", "timeline/TimeLineController"], function(jQuery, TimeLineController) {
-			
-			
-			
 			
 			var oTimeLineController = new TimeLineController();
 			var eTimeLineContainer = $('.timeline_container');
@@ -19,36 +14,9 @@ require( ["plugins/domReady"], function(domReady){
 			oTimeLineController.addTimeLineToView( "topline" );
 			oTimeLineController.renderTimeLines();
 			
-			
-			
-			/*
-			var oTimeLine = new TimeLine();
-			var eTimeLine = oTimeLine.getElement();
-			
-			
-			eTimeLineContainer.append( eTimeLine );
-			
-			var fResize = function() {
-				eTimeLine.css('top', (eTimeLineContainer.height()/2)+"px" );
-				console.log("resizing");
-			};
-			$(window).resize( fResize );
-			fResize();
-			*/
-			
-		
-			
 		});
 		
-//		console.log("loaded");
-//		document.addEventListener('touchmove', function(e){ e.preventDefault(); });
-//		myScroll = new iScroll('scroller', { checkDOMChanges: false, bounce: true, momentum: true });
-//		myScroll.refresh();
 	});
-	//This function is called when scripts/helper/util.js is loaded.
-    //If util.js calls define(), then this function is not fired until
-    //util's dependencies have loaded, and the util argument will hold
-    //the module value for "helper/util".
-	//sugoi
+
 });
 
